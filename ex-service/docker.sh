74ca6d3a201e69cb2d4ee66c9bf44ac95286b5eb
ianhsu-softchef <ianhsu@softchef.com>
2022-05-24 09:22:43 +0800

    05/23

--- ex-service/docker.sh
+++ ex-service/docker.sh
@@ -1,1 +1,18 @@
+apt-get update
+apt-get upgrade
+apt-get install git
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+
+git clone https://github.com/ahnochen/project.git
+
+cd project
+
+# RUN npm install
+npm install
+
+cd ..
+
 node index.js
